30085168ac228aa7c74fe966814b7fb8ab2c5303
matthieudou <matthieu.doultremont@gmail.com>
2018-02-24 18:00:03 +0100

    sidebar done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+#app.min-h-screen.bg-grey.pt-8.pb-8
+  <router-view/>
 </template>
 
 <script>
+import '@/assets/styles/main.scss'
+
 export default {
-  name: 'App'
+
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.container.flex.bg-white.mx-auto.shadow-lg
+  //-
+  .min-h-full.text-white(class="w-1/3")
+    Sidebar.bg-blue
+  .min-h-full(class="w-2/3")
+
+</template>
+
+<script>
+import Sidebar from '@/components/resume/Sidebar'
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
