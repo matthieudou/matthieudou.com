e9ac2649d8ee041aa8fb81c7305974bea0591ebb
matthieudou <matthieu.doultremont@gmail.com>
2018-04-29 18:06:49 +0200

    pug to html for purge css to work

--- components/resume/MainContent.vue
+++ components/resume/MainContent.vue
@@ -1,110 +1,124 @@
-<template lang="pug">
-.content
-  .shadow-md.p-4.m-1
-    .flex.items-center
-      <icon width="1rem" height="1rem" scale="2" name="school" class="mr-2"></icon>
-      h3.text-blue Formations
-    p.mt-4
-      h4
-        | Le Wagon Bootcamp
-        span.text-grey-darker  (juin 2017)
-      p.text-grey-darker FullStack Developer, Web Development
-    p.mt-4
-      h4
-        | Technologie de l’informatique
-        span.text-grey-darker  (sep 2016 - jan 2017)
-      p.text-grey-darker Ephec Louvain-la-Neuve | 1<sup>ère</sup> bac
-    p.mt-4
-      h4
-        | Informatique de gestion
-        span.text-grey-darker  (sep 2014 - juin 2016)
-      p.text-grey-darker Institut Paul Lambin | 1<sup>ère</sup> bac
-    p.mt-4
-      h4
-        | Brevet animateur
-        span.text-grey-darker  (mars 2014)
-      p.text-grey-darker ONE | Les Scouts ASBL
-    p.mt-4
-      h4
-        | Humanités
-        span.text-grey-darker  (juin 2014)
-      p.text-grey-darker Sciences 6 - Anglais 4 | Institut de l’Assomption
-  .flex.mt-4
-    .shadow-md.m-1.p-4(class="w-1/2")
-      .flex.items-center
-        <icon width="1rem" height="1rem" scale="2" name="language" class="mr-2"></icon>
-        h3.text-blue Langues
-      p.mt-4
-        h4
-          | Français
-        p.text-grey-darker Langue maternelle
-      p.mt-4
-        h4
-          | Néerlandais
-        p.text-grey-darker Compétence professionnelle
-      p.mt-4
-        h4
-          | Anglais
-        p.text-grey-darker Compétence professionnelle
-    .shadow-md.m-1.p-4(class="w-1/2")
-      .flex.items-center
+<template>
+  <div class="content">
+    <div class="shadow-md p-4 m-1">
+      <div class="flex items-center">
+        <icon width="1rem" height="1rem" scale="2" name="school" class="mr-2"></icon>
+        <h3 class="text-blue">Formations</h3>
+      </div>
+      <div class="mt-4">
+        <h4>Le Wagon Bootcamp<span class="text-grey-darker"> (juin 2017)</span></h4>
+        <p class="text-grey-darker">FullStack Developer, Web Development</p>
+      </div>
+      <div class="mt-4">
+        <h4>Technologie de l’informatique<span class="text-grey-darker"> (sep 2016 - jan 2017)</span></h4>
+        <p class="text-grey-darker">Ephec Louvain-la-Neuve | 1<sup>ère</sup> bac</p>
+      </div>
+      <div class="mt-4">
+        <h4>Informatique de gestion<span class="text-grey-darker"> (sep 2014 - juin 2016)</span></h4>
+        <p class="text-grey-darker">Institut Paul Lambin | 1<sup>ère</sup> bac</p>
+      </div>
+      <div class="mt-4">
+        <h4>Brevet animateur<span class="text-grey-darker"> (mars 2014)</span></h4>
+        <p class="text-grey-darker">ONE | Les Scouts ASBL</p>
+      </div>
+      <div class="mt-4">
+        <h4>Humanités<span class="text-grey-darker"> (juin 2014)</span></h4>
+        <p class="text-grey-darker">Sciences 6 - Anglais 4 | Institut de l’Assomption</p>
+      </div>
+    </div>
+    <div class="flex mt-4">
+      <div class="shadow-md m-1 p-4 w-1/2">
+        <div class="flex items-center">
+          <icon width="1rem" height="1rem" scale="2" name="language" class="mr-2"></icon>
+          <h3 class="text-blue">Langues</h3>
+        </div>
+        <div class="mt-4">
+          <h4>Français</h4>
+          <p class="text-grey-darker">Langue maternelle</p>
+        </div>
+        <div class="mt-4">
+          <h4>Néerlandais</h4>
+          <p class="text-grey-darker">Compétence professionnelle</p>
+        </div>
+        <div class="mt-4">
+          <h4>Anglais</h4>
+          <p class="text-grey-darker">Compétence professionnelle</p>
+        </div>
+      </div>
+    <div class="shadow-md m-1 p-4 w-1/2">
+      <div class="flex items-center">
         <icon width="1rem" height="1rem" scale="2" name="code" class="mr-2"></icon>
-        h3.text-blue Compétences
-      p.mt-4
-        ul
-          li HTML 5 & CSS 3
-          li Javascript (ES6)
-          li Ruby
-          li Rspec
-          li
-            | Frameworks
-            ul
-              li Rails
-              li Vue.js
-              li Nuxt.js
-  .shadow-md.m-1.p-4.mt-4
-    .flex.items-center
-      <icon width="1rem" height="1rem" scale="2" name="briefcase" class="mr-2"></icon>
-      h3.text-blue Expérience
-    p.mt-4
-      h4
-        | Fullstack developer
-      p.text-grey-darker Smovin (avr. 2018 - Actuellement)
-    p.mt-4
-      h4
-        | Fullstack developer
-      p.text-grey-darker Seraphin SA (sep. 2017 - fev. 1018)
-    p.mt-4
-      h4
-        | Monteur de photomaton
-      p.text-grey-darker CheeseBox (2016 - 2017)
-    p.mt-4
-      h4
-        | Caisse/Mise en rayon
-      p.text-grey-darker Colruyt (oct. 2015 - avr. 2016)
-    p.mt-4
-      h4
-        | Vendeur comptoir
-      p.text-grey-darker Carodec (août 2014)
-    p.mt-4
-      h4
-        | Rénovation toiture
-      p.text-grey-darker Elico (août 2013)
-  .shadow-md.m-1.p-4.mt-4
-    .flex.items-center
-      <icon width="1rem" height="1rem" scale="2" name="plane" class="mr-2"></icon>
-      h3.text-blue Loisirs
-    p.mt-4 Scoutisme, menuiserie, ski, bière, bonne nourriture, sports nautiques, open source, musique
-  .shadow-md.m-1.p-4.bg-blue.flex.mt-4.text-white
-    .flex.items-center(class="w-1/2")
-      <h4>
-        <icon width="1rem" height="1rem" scale="2" name="github" class="mr-2"></icon>
-        <a class="text-white" href="https://github.com/matthieudou">matthieudou</a>
-      </h4>
-    .flex.items-center(class="w-1/2")
-      <icon width="1rem" height="1rem" scale="2" name="linkedin" class="mr-2"></icon>
-      h4
-        a.text-white(href="https://www.linkedin.com/in/matthieudou/") Matthieu d'Oultremont
+        <h3 class="text-blue">Compétences</h3>
+      </div>
+      <p class="mt-4">
+        <ul>
+          <li>HTML 5 & CSS 3</li>
+          <li>Javascript (ES6)</li>
+          <li>Ruby</li>
+          <li>Rspec</li>
+          <li>Frameworks
+            <ul>
+              <li>Rails</li>
+              <li>Vue.js</li>
+              <li>Nuxt.js</li>
+            </ul>
+          </li>
+        </ul>
+      </p>
+    </div>
+    </div>
+    <div class="shadow-md m-1 p-4 mt-4">
+      <div class="flex items-center">
+        <icon width="1rem" height="1rem" scale="2" name="briefcase" class="mr-2"></icon>
+        <h3 class="text-blue">Expérience</h3>
+      </div>
+      <div class="mt-4">
+        <h4>Fullstack developer</h4>
+        <p class="text-grey-darker">Smovin (avr. 2018 - Actuellement)</p>
+      </div>
+      <div class="mt-4">
+        <h4>Fullstack developer</h4>
+        <p class="text-grey-darker">Seraphin SA (sep. 2017 - fev. 1018)</p>
+      </div>
+      <div class="mt-4">
+        <h4>Monteur de photomaton</h4>
+        <p class="text-grey-darker">CheeseBox (2016 - 2017)</p>
+      </div>
+      <div class="mt-4">
+        <h4>Caisse/Mise en rayon</h4>
+        <p class="text-grey-darker">Colruyt (oct. 2015 - avr. 2016)</p>
+      </div>
+      <div class="mt-4">
+        <h4>Vendeur comptoir</h4>
+        <p class="text-grey-darker">Carodec (août 2014)</p>
+      </div>
+      <div class="mt-4">
+        <h4>Rénovation toiture</h4>
+        <p class="text-grey-darker">Elico (août 2013)</p>
+      </div>
+    </div>
+    <div class="shadow-md m-1 p-4 mt-4">
+      <div class="flex items-center">
+        <icon width="1rem" height="1rem" scale="2" name="plane" class="mr-2"></icon>
+        <h3 class="text-blue">Loisirs</h3>
+      </div>
+      <p class="mt-4">Scoutisme, menuiserie, ski, bière, bonne nourriture, sports nautiques, open source, musique</p>
+    </div>
+    <div class="shadow-md m-1 p-4 bg-blue flex mt-4 text-white">
+      <div class="flex items-center w-1/2">
+        <h4>
+          <icon width="1rem" height="1rem" scale="2" name="github" class="mr-2"></icon>
+          <a class="text-white" href="https://github.com/matthieudou">matthieudou</a>
+        </h4>
+      </div>
+      <div class="flex items-center w-1/2">
+        <h4>
+          <icon width="1rem" height="1rem" scale="2" name="linkedin" class="mr-2"></icon>
+          <a class="text-white" href="https://www.linkedin.com/in/matthieudou/">Matthieu d'Oultremont</a>
+        </h4>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- components/resume/Sidebar.vue
+++ components/resume/Sidebar.vue
@@ -1,24 +1,27 @@
-<template lang="pug">
-div
-  img(src="/profil.jpg")
-  .p-2
-    h1.font-hairline.text-center.uppercase Matthieu d'Oultremont
-    h2.font-hairline.text-center.uppercase Junior Fullstack developer
-    .border-t-2.border-blue-dark.py-4.px-2.mt-4.flex.items-center
-      <icon name="calendar" width="1rem" height="1rem" class="mr-2"></icon>
-      div 12/10/1994
-    .border-t-2.border-blue-dark.py-4.px-2.flex.items-center
-      <icon name="mail" width="1rem" height="1rem" class="mr-2"></icon>
-      div matthieu.doultremont@gmail.com
-    .border-t-2.border-blue-dark.py-4.px-2.flex.items-center
-      <icon name="map-marker" width="1rem" height="1rem" class="mr-2"></icon>
-      div Rue du Craetveld 91, <br>1120 Bruxelles
-    .border-t-2.border-blue-dark.py-4.px-2.flex.items-center
-      <icon name="phone" width="1rem" height="1rem" class="mr-2"></icon>
-      div 0471 30 46 75
-    .border-t-2.border-blue-dark.py-4.px-2.flex
-      <icon name="user" width="1rem" height="1rem" scale="6" class="mr-2"></icon>
-      .text-justify Animateur d'unité aux ardents de St-Michel, amateur de bonne nourriture et même de bière, passionné par le développement web, épris de menuiserie et randonneur. Voilà un petit avant-goût de la personne que je suis !
+<template>
+  <div>
+    <img src="/profil.jpg" alt="Matthieu d'Oultremont">
+    <div class="p-2">
+      <h1 class="font-hairline text-center uppercase">Matthieu d'Oultremont</h1>
+      <h2 class="font-hairline text-center uppercase">Fullstack developer</h2>
+      <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
+        <icon name="calendar" width="1rem" height="1rem" class="mr-2"></icon>
+        <span>12/10/1994</span>
+      </div>
+      <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
+        <icon name="mail" width="1rem" height="1rem" class="mr-2"></icon>
+        <span>matthieu.doultremont@gmail.com</span>
+      </div>
+      <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
+        <icon name="map-marker" width="1rem" height="1rem" class="mr-2"></icon>
+        <span>Rue du Craetveld 91, <br>1120 Bruxelles</span>
+      </div>
+      <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
+        <icon name="user" width="1rem" height="1rem" scale="6" class="mr-2"></icon>
+        <p>Animateur d'unité aux ardents de St-Michel, amateur de bonne nourriture et même de bière, passionné par le développement web, épris de menuiserie et randonneur. Voilà un petit avant-goût de la personne que je suis !</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
