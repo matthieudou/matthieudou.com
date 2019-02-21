22dcb6d3101ba1cadbae326e98c8bcc4ca18f1d7
matthieudou <matthieu.doultremont@gmail.com>
2019-02-21 16:15:50 +0100

    some advancement

--- components/resume/Sidebar.vue
+++ components/resume/Sidebar.vue
@@ -5,19 +5,19 @@
       <h1 class="font-hairline text-center uppercase">Matthieu d'Oultremont</h1>
       <h2 class="font-hairline text-center uppercase">Fullstack developer</h2>
       <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
-        <icon name="calendar" width="1rem" height="1rem" class="mr-2"></icon>
+        <icon name="calendar" class="w-4 mr-2"></icon>
         <span>12/10/1994</span>
       </div>
       <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
-        <icon name="mail" width="1rem" height="1rem" class="mr-2"></icon>
+        <icon name="mail" class="w-4 mr-2"></icon>
         <span>matthieu.doultremont@gmail.com</span>
       </div>
       <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
-        <icon name="map-marker" width="1rem" height="1rem" class="mr-2"></icon>
+        <icon name="map-marker" class="w-4 mr-2"></icon>
         <span>Rue du Craetveld 91, <br>1120 Bruxelles</span>
       </div>
       <div class="border-t-2 border-blue-dark py-4 px-2 mt-4 flex items-center">
-        <icon name="user" width="1rem" height="1rem" scale="6" class="mr-2"></icon>
+        <icon name="user" class="w-64 mr-2"></icon>
         <p>Animateur d'unité aux ardents de St-Michel, amateur de bonne nourriture et même de bière, passionné par le développement web, épris de menuiserie et randonneur. Voilà un petit avant-goût de la personne que je suis !</p>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -867,7 +867,7 @@ module.exports = {
     pointerEvents: ['responsive'],
     position: ['responsive'],
     resize: ['responsive'],
-    shadows: ['responsive'],
+    shadows: ['responsive', 'hover'],
     svgFill: [],
     svgStroke: [],
     textAlign: ['responsive'],
