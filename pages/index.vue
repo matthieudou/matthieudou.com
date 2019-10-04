8cef198d756f80d17fbb99d17f1019f904e191b1
matthieudou <matthieu.doultremont@gmail.com>
2019-10-04 16:23:11 +0200

    add identity to website

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,11 @@
 
 <script>
   export default {
-
+    head () {
+      return {
+        script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      }
+    }
   }
 </script>
 
