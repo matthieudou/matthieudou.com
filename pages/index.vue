293c464ef045b47d1f3e26d2ff6f56d21e451298
matthieudou <matthieu.doultremont@gmail.com>
2019-10-04 16:06:55 +0200

    reinitialize project

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        www
+      </h1>
+      <h2 class="subtitle">
+        Website of Matthieu d&#39;Oultremont
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
