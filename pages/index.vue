9c29586838ad5830b3bb29844a6944c8d7ebacac
matthieudou <matthieu.doultremont@gmail.com>
2019-11-28 22:40:07 +0100

    minimal phrase

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <main class="container mx-auto">
-    Hi 👋,
-    <br>
-    I'm Matthieu d'Oultremont, fullstack developer, amateur photograph and woodworking enthousiast.
+    <div>
+      <div>
+        Hi 👋,
+        <br>
+        I'm Matthieu d'Oultremont, fullstack developer, amateur photograph and woodworking enthousiast.
+      </div>
+    </div>
   </main>
 </template>
 
