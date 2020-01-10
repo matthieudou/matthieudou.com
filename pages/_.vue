a4809f84ed60b56e1d3ff397b5dca05724835002
matthieudou <matthieu.doultremont@gmail.com>
2020-01-10 15:21:00 +0100

    initialization of sanity

---
+++ pages/_.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+  export default {
+    layout () {
+
+    },
+
+    mounted () {
+      console.log(this.$route.params)
+    }
+  }
+</script>
